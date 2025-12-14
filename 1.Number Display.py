ea26e50975787164f8eae8b0e958ad1dae78d952
IOLANTHE0116 <145015482+IOLANTHE0116@users.noreply.github.com>
2025-12-14 19:27:05 +0800

    Add files via upload

---
+++ 1.Number Display.py
@@ -0,0 +1,9 @@
+from microbit import *
+one = Image(":00900:09900:00900:99999")
+display.show(one)
+
+two = Image(":99900:00090:99900:99990")
+display.show(two)
+
+three = Image(":09900:90090:90090:09900")
+display.show(three)
